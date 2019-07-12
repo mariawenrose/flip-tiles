9687e8b1907914019d9b541baff3623693ba8517
mariawenrose <mariarose.wen@gmail.com>
2019-07-12 16:49:29 +1200

    thank you siri

--- js/sprint3-technical.js
+++ js/sprint3-technical.js
@@ -1,80 +1,17 @@
-console.log('test')
+var checkboxes = document.getElementsByName("checkbox");
 
-function displayDiv() {
-  var checkBox1 = document.getElementById('cb-1');
-  var textDiv1 = document.getElementById('td-1');
+function addListeners() { 
 
-  if (checkBox1.checked == true) {
-    textDiv1.style.display = "block" 
-      
-    } else {
-    
-      textDiv1.style.display = "none";
+  for (var i = 0; i < checkboxes.length; i++) {  
+    checkboxes[i].addEventListener( 'change', function(evt) {
+        var idNumber = evt.target.id[3]
+        var div = document.getElementById('td-' + idNumber)
+        if(evt.target.checked) {
+          div.style.display = "block"
+        } else {
+          div.style.display = "none";
+        }
+    });
   }
 }
-
-// function displayDiv() {
-//   var checkBox = document.getElementsByClassName('c-box');
-//   var textDiv = document.getElementsByClassName('text-div');
-
-//   if (checkBox.checked == true) {
-//     textDiv.style.display = "block" 
-      
-//     } else {
-    
-//       textDiv.style.display = "none";
-//   }
-// }
-
-
-
-// function Validate() {
-//   var checked = 0;
-
-
-//   //Reference all the CheckBoxes in Table.
-//   var chks = checkbox-wrapper.getElementsByClassName('c-box');
-//   console.log(chks)
-
-//   //Loop and count the number of checked CheckBoxes.
-//   for (var i = 0; i < chks.length; i++) {
-//       if (chks[i].checked) {
-//           checked++;
-//       }
-//   }
-
-//   if (checked > 0) {
-//       alert(checked + " CheckBoxe(s) are checked.");
-//       return true;
-//   } else {
-//       alert("Please select CheckBoxe(s).");
-//       return false;
-//   }
-// };
-
-var checkbox = document.querySelector("input[name=checkbox]");
-var textDiv = document.getElementsByClassName('text-div');
-
-
-checkbox.addEventListener( 'change', function() {
-    if(checkbox.checked) {
-      // textDiv.style.display = "block"
-      console.log('hi')
-    } else {
-      // textDiv.style.display = "none";
-      console.log('no')
-
-    }
-});
-
-
-// console.log(document.getElementsByClassName('c-box'))
-
-
-//   if (checkBox.checked == true) {
-//     textDiv.style.display = "block" 
-      
-//     } else {
-    
-//       textDiv.style.display = "none";
-//   }
+addListeners()
